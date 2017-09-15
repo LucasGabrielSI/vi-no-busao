e2a03ba6d19db3fff0ee7c392eed8790d3074c4c
Lucas Gabriel <lucasiguape16@gmail.com>
2017-09-15 18:37:03 -0300

    Adiciona classe para onibus

---
+++ Onibus.java
@@ -0,0 +1,18 @@
+package VInoBusao;
+
+import javax.swing.JOptionPane;
+
+public class Onibus {
+	String rota;
+	String hora;
+	Usuario usuario;
+		
+	public void listarUsuariosOnline() {
+		JOptionPane.showMessageDialog(null ,"Usuários online:\r\n \r\n"
+				+ " Artur bruno \r\n"
+				+ " Rayane correa \r\n"
+				+ " Diego Teixeira\r\n"
+				+ " Davi Trigueiro\r\n"
+				+ " Tendeu");
+	}
+}
